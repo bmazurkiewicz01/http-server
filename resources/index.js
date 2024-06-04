e5b216485335def2b32cff696a67a3771656cae7
bmazurkiewicz01 <bartek.mazurkiewicz69@gmail.com>
2024-06-04 23:47:14 +0200

    Adjusted index.js

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,8 @@
-const btn = document.getElementById('add-h2-btn');
-btn.addEventListener('click', () => {
-    const h2 = document.createElement('h2');
-    h2.textContent = 'test H2 dolaczony przez javascript';
-    document.body.appendChild(h2);
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('add-h2-btn');
+    btn.addEventListener('click', () => {
+        const h2 = document.createElement('h2');
+        h2.textContent = 'test H2 dolaczony przez javascript';
+        document.body.appendChild(h2);
+    });
 });
